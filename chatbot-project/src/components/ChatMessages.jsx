8f67f406946bcc8d41e6af793d85880130d3b47a
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-09-15 21:49:02 +0530

    Initialize chatbot project with React and Vite setup
    Sets up a chatbot project using React and Vite, including essential configurations, dependencies, and initial components. Implements a basic chat interface with message handling, auto-scroll, and loading feedback. Includes ESLint setup for code quality, a `.gitignore` file, and a README for project guidance. Removes unused assets and legacy files.

---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+import { ChatMessage } from "./ChatMessage";
+import "./ChatMessages.css";
+// custom hook - auto scroll.........
+function useAutoScroll(dependencies) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = containerRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, dependencies);
+
+  return containerRef;
+}
+
+export function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((cm, key) => {
+        return (
+          <ChatMessage message={cm.message} sender={cm.sender} key={key} />
+        );
+      })}
+    </div>
+  );
+}
