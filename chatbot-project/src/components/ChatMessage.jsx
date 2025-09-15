2ad90d72dd3b3f193d15e82db0396f3a663290b5
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-09-15 22:57:41 +0530

    5i done. added message timer for specific messages using dayjs module

--- chatbot-project/src/components/ChatMessage.jsx
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -1,8 +1,10 @@
 import RobotProfileImage from "../assets/robot.png";
 import UserProfileImage from "../assets/lebloom.png";
+import dayjs from "dayjs";
 import "./ChatMessage.css";
 
 export function ChatMessage({ message, sender }) {
+  const time = dayjs().valueOf();
   return (
     <div
       className={
@@ -12,7 +14,10 @@ export function ChatMessage({ message, sender }) {
       {sender === "robot" && (
         <img className="chat-image" src={RobotProfileImage} />
       )}
-      <div className="chat-message-text">{message}</div>
+      <div className="chat-message-text">
+        {message} <br />
+        <p className="chat-time">{dayjs(time).format("h.mma")}</p>
+      </div>
       {sender === "user" && (
         <img className="chat-image" src={UserProfileImage} />
       )}
